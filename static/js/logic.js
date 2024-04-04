6dce0d2c2c0555959d6fc7e2f9460b946d93ca1c
Singer2319 <147009190+Singer2319@users.noreply.github.com>
2024-04-04 04:21:38 -0700

    Add files via upload
    set up map, working on turning it into like... circles

---
+++ static/js/logic.js
@@ -0,0 +1,64 @@
+//Part 1: Create the Earthquake Visualization
+
+//set up the map
+//play around with lat and lon coordinates to center over west usa
+//lon is north south, lat is east west
+the_map = L.map("map", {center: [40, -110], zoom : 5});
+
+//copy paste tile layer from other js file
+// adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(the_map);
+
+//set up the url
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+//get the data from the url
+d3.json(url).then(function(data){
+
+    console.log(data)
+
+    //marker_list = [];
+
+    //note: the data has it lon then lat so reverse it
+    dfeatures = data.features;
+
+    // for (i = 0; i < 1500; i++) {
+
+    //     dfgeometry = dfeatures[i].geometry;
+
+    //     if (dfeatures[i].geometry)
+    //         //console.log(`Lon: ${indexin[i].geometry[1]}`)
+    //         L.marker([dfgeometry.coordinates[1], dfgeometry.coordinates[0]]).addTo(the_map);
+    // };
+
+    smthn = L.choropleth(data, {
+        valueProperty : "ids", 
+        scale : ["#ffffb2", "#b10026"], 
+        steps : 5,
+        mode : "q",
+        style : {
+          // Border color
+          color: "#fff",
+          weight: 1,
+          fillOpacity: 0.8
+        },
+        onEachFeature : function(feature, layer){
+            layer.bindPopup("<strong> Earthquake Magnitude: " + feature.properties.mag + "</strong>" 
+            + "<br /><br />" + "Located Around: " + feature.properties.place)
+        }
+        // onEachFeature: function(feature, layer) {
+        //     layer.bindPopup("<strong>" + feature.properties.NAME + "</strong><br /><br />Estimated employed population with children age 6-17: " +
+        //       feature.properties.DP03_16E + "<br /><br />Estimated Total Income and Benefits for Families: $" + feature.properties.DP03_75E);
+        //   }
+        // }).addTo(myMap);
+      
+
+    }).addTo(the_map)
+
+});
+
+
+
+
